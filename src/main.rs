9f18e9533daa05c13bdced76a0293c6b7640fe73
Zenios Zeniou <mrzenioszeniou@gmail.com>
2021-12-05 19:03:43 +0100

    Fix usage

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ fn main() {
     let args: Vec<String> = std::env::args().collect();
 
     if args.len() != 2 {
-        println!("Usage: adventofcode2019 DAY");
+        println!("Usage: adventofcode2021 DAY");
         std::process::exit(1)
     }
 

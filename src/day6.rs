27d46518ffd11b3f87173a62fff4ee6ca1cfd54a
Zenios Zeniou <mrzenioszeniou@gmail.com>
2021-12-06 16:24:06 +0100

    Cleanup

--- src/day6.rs
+++ src/day6.rs
@@ -1,44 +1,26 @@
-use std::collections::HashMap;
-
 pub fn solve() -> (usize, usize) {
     let school = parse();
 
     (simulator(school.clone(), 80), simulator(school, 256))
 }
 
-fn simulator(mut school: HashMap<u8, usize>, days: usize) -> usize {
+fn simulator(mut school: Vec<usize>, days: usize) -> usize {
     for _ in 0..days {
-        let mut next = HashMap::new();
-
-        for (age, count) in school.into_iter() {
-            if age == 0 {
-                next.entry(6)
-                    .and_modify(|cnt| *cnt += count)
-                    .or_insert(count);
-                next.entry(8)
-                    .and_modify(|cnt| *cnt += count)
-                    .or_insert(count);
-            } else {
-                next.entry(age - 1)
-                    .and_modify(|cnt| *cnt += count)
-                    .or_insert(count);
-            }
-        }
-
-        school = next;
+        school[7] += school[0];
+        school.rotate_left(1);
     }
 
-    school.values().sum()
+    school.iter().sum()
 }
 
-fn parse() -> HashMap<u8, usize> {
+fn parse() -> Vec<usize> {
     std::fs::read_to_string("res/day6.txt")
         .unwrap()
         .split(',')
-        .map(|s| s.parse().unwrap())
-        .fold(HashMap::new(), |mut map, fish| {
-            map.entry(fish).and_modify(|cnt| *cnt += 1).or_insert(1);
-            map
+        .map(|s| s.parse::<usize>().unwrap())
+        .fold(vec![0; 9], |mut acc, fish| {
+            acc[fish] += 1;
+            acc
         })
 }
 
@@ -48,30 +30,21 @@ mod tests {
 
     #[test]
     fn example0() {
-        assert_eq!(simulator(HashMap::from([(3, 1)]), 5), 2);
+        assert_eq!(simulator(vec![0, 0, 0, 1, 0, 0, 0, 0, 0], 5), 2);
     }
 
     #[test]
     fn example1() {
-        assert_eq!(
-            simulator(HashMap::from([(1, 1), (2, 1), (3, 2), (4, 1)]), 18),
-            26
-        );
+        assert_eq!(simulator(vec![0, 1, 1, 2, 1, 0, 0, 0, 0], 18), 26);
     }
 
     #[test]
     fn example2() {
-        assert_eq!(
-            simulator(HashMap::from([(1, 1), (2, 1), (3, 2), (4, 1)]), 80),
-            5934
-        );
+        assert_eq!(simulator(vec![0, 1, 1, 2, 1, 0, 0, 0, 0], 80), 5934);
     }
 
     #[test]
     fn example3() {
-        assert_eq!(
-            simulator(HashMap::from([(1, 1), (2, 1), (3, 2), (4, 1)]), 256),
-            26984457539
-        );
+        assert_eq!(simulator(vec![0, 1, 1, 2, 1, 0, 0, 0, 0], 256), 26984457539);
     }
 }

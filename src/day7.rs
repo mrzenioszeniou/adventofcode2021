5dc2b686f5704714597520aaa099ff2d389372c7
Zenios Zeniou <mrzenioszeniou@gmail.com>
2021-12-07 14:07:09 +0100

    Cleanup

--- src/day7.rs
+++ src/day7.rs
@@ -3,45 +3,22 @@ use std::cmp::Ordering;
 pub fn solve() -> (isize, isize) {
     let crabs = parse();
 
-    (part1(&crabs), part2(&crabs))
+    (
+        solver(&crabs, |from, to| (to - from).abs()),
+        solver(&crabs, |from, to| {
+            let dis = (to - from).abs();
+            dis * (dis + 1) / 2
+        }),
+    )
 }
 
-fn part1(crabs: &[isize]) -> isize {
+fn solver(crabs: &[isize], fuel_calc: impl Fn(isize, isize) -> isize) -> isize {
     let mut curr = crabs[crabs.len() / 2];
-    let mut curr_fuel = crabs.iter().map(|c| (c - curr).abs()).sum();
+    let mut curr_fuel = crabs.iter().map(|c| fuel_calc(*c, curr)).sum();
 
     loop {
-        let left_fuel: isize = crabs.iter().map(|c| (c - (curr - 1)).abs()).sum();
-        let right_fuel: isize = crabs.iter().map(|c| (c - (curr + 1)).abs()).sum();
-
-        match (left_fuel.cmp(&curr_fuel), right_fuel.cmp(&curr_fuel)) {
-            (Ordering::Equal, Ordering::Equal) | (Ordering::Less, Ordering::Less) => {
-                panic!("What do I do?")
-            }
-            (Ordering::Greater, Ordering::Greater) => return curr_fuel,
-            (Ordering::Less, Ordering::Equal)
-            | (Ordering::Less, Ordering::Greater)
-            | (Ordering::Equal, Ordering::Greater) => {
-                curr -= 1;
-                curr_fuel = left_fuel;
-            }
-            (Ordering::Equal, Ordering::Less)
-            | (Ordering::Greater, Ordering::Less)
-            | (Ordering::Greater, Ordering::Equal) => {
-                curr += 1;
-                curr_fuel = right_fuel;
-            }
-        }
-    }
-}
-
-fn part2(crabs: &[isize]) -> isize {
-    let mut curr = crabs[crabs.len() / 2];
-    let mut curr_fuel = crabs.iter().map(|c| fuel(*c, curr)).sum();
-
-    loop {
-        let left_fuel: isize = crabs.iter().map(|c| fuel(*c, curr - 1)).sum();
-        let right_fuel: isize = crabs.iter().map(|c| fuel(*c, curr + 1)).sum();
+        let left_fuel: isize = crabs.iter().map(|c| fuel_calc(*c, curr - 1)).sum();
+        let right_fuel: isize = crabs.iter().map(|c| fuel_calc(*c, curr + 1)).sum();
 
         match (left_fuel.cmp(&curr_fuel), right_fuel.cmp(&curr_fuel)) {
             (Ordering::Equal, Ordering::Equal) | (Ordering::Less, Ordering::Less) => {
@@ -70,9 +47,4 @@ fn parse() -> Vec<isize> {
         .split(',')
         .map(|s| s.parse().unwrap())
         .collect()
-}
-
-fn fuel(from: isize, to: isize) -> isize {
-    let dis = (to - from).abs();
-    dis * (dis + 1) / 2
 }

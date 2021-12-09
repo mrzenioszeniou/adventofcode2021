9c77cfccb6ccd2e07f1e29bf3a641536682eeec3
Zenios Zeniou <mrzenioszeniou@gmail.com>
2021-12-09 14:25:08 +0100

    Cleanup

--- src/day9.rs
+++ src/day9.rs
@@ -1,6 +1,5 @@
+use crate::dir::neighbours_bounded;
 use std::collections::{BTreeSet, HashSet};
-
-use crate::dir::Direction;
 
 pub fn solve() -> (u32, usize) {
     let heightmap = parse();
@@ -26,7 +25,7 @@ fn part2(heightmap: &[Vec<u32>]) -> usize {
             let mut to_visit = BTreeSet::from([low]);
 
             while let Some(pos) = to_visit.pop_first() {
-                neighbours(pos, n, m)
+                neighbours_bounded(pos, n, m)
                     .into_iter()
                     .filter(|n| !visited.contains(n) && heightmap[n.0][n.1] != 9)
                     .for_each(|n| {
@@ -46,35 +45,17 @@ fn part2(heightmap: &[Vec<u32>]) -> usize {
     basins.pop().unwrap() * basins.pop().unwrap() * basins.pop().unwrap()
 }
 
-fn neighbours(pos: (usize, usize), n: usize, m: usize) -> Vec<(usize, usize)> {
-    let pos = (pos.0 as isize, pos.1 as isize);
-    let n = n as isize;
-    let m = m as isize;
-
-    [
-        Direction::North,
-        Direction::South,
-        Direction::East,
-        Direction::West,
-    ]
-    .iter()
-    .map(|dir| {
-        let step = dir.forward();
-        (pos.0 + step.0, pos.1 + step.1)
-    })
-    .filter(|&(i, j)| i >= 0 && i < n && j >= 0 && j < m)
-    .map(|(i, j)| (i as usize, j as usize))
-    .collect()
-}
-
 fn find_lowpoints(heightmap: &[Vec<u32>]) -> HashSet<(usize, usize)> {
     let mut ret = HashSet::new();
 
-    for i in 0..heightmap.len() {
-        for j in 0..heightmap[i].len() {
+    let n = heightmap.len();
+    let m = heightmap[0].len();
+
+    for i in 0..n {
+        for j in 0..m {
             let height = heightmap[i][j];
 
-            if neighbours((i, j), heightmap.len(), heightmap[i].len())
+            if neighbours_bounded((i, j), n, m)
                 .into_iter()
                 .all(|(i, j)| heightmap[i][j] > height)
             {
--- src/dir.rs
+++ src/dir.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 #[derive(Clone, Copy, Debug)]
 pub enum Direction {
     North,
@@ -15,4 +17,29 @@ impl Direction {
             Self::West => (0, -1),
         }
     }
+
+    pub fn iter_all() -> impl Iterator<Item = Self> {
+        [
+            Direction::North,
+            Direction::South,
+            Direction::East,
+            Direction::West,
+        ]
+        .into_iter()
+    }
+}
+
+pub fn neighbours_bounded(pos: (usize, usize), n: usize, m: usize) -> HashSet<(usize, usize)> {
+    let pos = (pos.0 as isize, pos.1 as isize);
+    let n = n as isize;
+    let m = m as isize;
+
+    Direction::iter_all()
+        .map(|dir| {
+            let step = dir.forward();
+            (pos.0 + step.0, pos.1 + step.1)
+        })
+        .filter(|&(i, j)| i >= 0 && i < n && j >= 0 && j < m)
+        .map(|(i, j)| (i as usize, j as usize))
+        .collect()
 }

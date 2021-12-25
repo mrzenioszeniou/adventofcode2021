566b72de8dc7cacfcf15bba8242b3c60f2bff1a7
Zenios Zeniou <mrzenioszeniou@gmail.com>
2021-12-25 16:46:57 +0100

    Update day24.rs

--- src/day24.rs
+++ src/day24.rs
@@ -1,3 +1,20 @@
 pub fn solve() -> (usize, usize) {
     (42,42)
 }
+
+struct ALU {
+    w: isize,
+    x: isize,
+    y: isize,
+    z: isize,
+    program: Vec<String>,
+
+}
+
+impl ALU {
+    
+    pub fn execute(input: Vec<isize>) -> isize {
+        
+    
+    }
+}

352fc1f4e9214eea2bbd0675af19be6f380b715f
Zenios Zeniou <mrzenioszeniou@gmail.com>
2021-12-25 23:43:59 +0100

    Day 24 - Commands needs simplifying

--- src/day24.rs
+++ src/day24.rs
@@ -1,20 +1,163 @@
-pub fn solve() -> (usize, usize) {
-    (42,42)
+pub fn solve() -> (isize, usize) {
+    let program = std::fs::read_to_string("res/day24.txt").unwrap();
+
+    (part1(&program), 42)
 }
 
+fn part1(program: &str) -> isize {
+    let alu = ALU::new(program);
+
+    search(alu)
+        .unwrap()
+        .into_iter()
+        .rev()
+        .enumerate()
+        .map(|(i, d)| d * 10_isize.pow(i as u32))
+        .sum()
+
+    // let mut input = vec![1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+    // loop {
+
+    //     for value in
+
+    //     println!("{:?}", input);
+
+    //     let mut alu = alu.clone();
+
+    //     alu.execute(input.clone());
+
+    //     if alu.done() && alu.z == 0 {
+    //         break;
+    //     } else {
+    //         for digit in input.iter_mut() {
+    //             *digit += 1;
+
+    //             if *digit > 9 {
+    //                 *digit = 1;
+    //             } else {
+    //                 break;
+    //             }
+    //         }
+    //     }
+    // }
+    // println!("Min found {:?}", input);
+
+    // let mut max = vec![9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9];
+    // panic!("No solution found");
+}
+
+fn search(alu: ALU) -> Option<Vec<isize>> {
+    println!("{:?}", alu.input);
+    for i in (1..10).rev() {
+        let mut alu = alu.clone();
+
+        alu.execute(vec![i]);
+
+        if alu.done() {
+            if alu.z == 0 {
+                return Some(vec![i]);
+            } else {
+                return None;
+            }
+        } else if let Some(mut res) = search(alu) {
+            res.push(i);
+            return Some(res);
+        }
+    }
+
+    None
+}
+
+#[derive(Clone)]
 struct ALU {
     w: isize,
     x: isize,
     y: isize,
     z: isize,
+    index: usize,
     program: Vec<String>,
-
+    input: Vec<isize>,
 }
 
 impl ALU {
-    
-    pub fn execute(input: Vec<isize>) -> isize {
-        
-    
+    pub fn new(from: &str) -> Self {
+        Self {
+            w: 0,
+            x: 0,
+            y: 0,
+            z: 0,
+            index: 0,
+            program: from
+                .split('\n')
+                .map(|s| s.trim().to_string())
+                .filter(|c| !c.is_empty())
+                .collect(),
+            input: vec![],
+        }
     }
+
+    fn done(&self) -> bool {
+        self.index == self.program.len()
+    }
+
+    fn register(&mut self, register: &str) -> &mut isize {
+        match register {
+            "w" => &mut self.w,
+            "x" => &mut self.x,
+            "y" => &mut self.y,
+            "z" => &mut self.z,
+            _ => panic!("Invalid register requested: '{}'", register),
+        }
+    }
+
+    fn value(&self, value: &str) -> isize {
+        match value {
+            "w" => self.w,
+            "x" => self.x,
+            "y" => self.y,
+            "z" => self.z,
+            n => n.parse().unwrap(),
+        }
+    }
+
+    pub fn execute(&mut self, mut input: Vec<isize>) {
+        while self.index < self.program.len() {
+            if let Some(r) = sscanf::scanf!(self.program[self.index], "inp {}", String) {
+                if let Some(input) = input.pop() {
+                    self.input.push(input);
+                    *self.register(&r) = input;
+                } else {
+                    return;
+                }
+            } else {
+                let (command, left, right) =
+                    sscanf::scanf!(self.program[self.index], "{} {} {}", String, String, String)
+                        .expect("Invalid command");
+
+                match command.as_str() {
+                    "mul" => *self.register(&left) = self.value(&left) * self.value(&right),
+                    "add" => *self.register(&left) = self.value(&left) + self.value(&right),
+                    "div" => *self.register(&left) = self.value(&left) / self.value(&right),
+                    "mod" => *self.register(&left) = self.value(&left) % self.value(&right),
+                    "eql" => {
+                        *self.register(&left) = if self.value(&left) == self.value(&right) {
+                            1
+                        } else {
+                            0
+                        }
+                    }
+                    _ => {}
+                }
+            }
+
+            self.index += 1;
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn examples() {}
 }
